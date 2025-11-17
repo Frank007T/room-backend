7b2e953477784fb8ddbdecb7a68b8b79772c2643
Frank007T <franklinoffcl07@gmail.com>
2025-11-18 00:45:51 +0530

    new

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
 // MongoDB Connection
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/room-admin')
+mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://franklinoffcl07_db_user:OQ3XLXocb0IcSDWt@cluster0.hnhb9kw.mongodb.net/')
   .then(() => console.log('MongoDB connected'))
   .catch(err => console.log('MongoDB connection error:', err));
 
